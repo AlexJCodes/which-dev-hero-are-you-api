24c26bb4246297c03d299806420c6e04d93ea967
AlexJCodes <alexander.johansson8910@gmail.com>
2026-05-21 19:49:31 +0200

    ✨ feature(submissions): Add submission data

---
+++ src/data/submissions.ts
@@ -0,0 +1,5 @@
+import type { Submission } from "../types/quiz.types"
+
+//Store submissions in-memory (could be replaced with a database in the future)
+
+export const submissions: Submission[] = []

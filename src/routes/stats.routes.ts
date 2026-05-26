a9cb5edd9127a37508e7e39aafb892ec3969d008
AlexJCodes <alexander.johansson8910@gmail.com>
2026-05-26 14:56:52 +0200

    ✨ feat: add quiz stats route

---
+++ src/routes/stats.routes.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express'
+
+import { submissions } from '../data/submissions'
+import { calculateStats } from '../utils/calculateStats'
+
+// Stat-related routes
+export const statsRouter = express.Router()
+
+// GET /stats
+statsRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const quizStats = calculateStats(submissions)
+
+        res.json(quizStats)
+    } catch (error) {
+        console.error(error)
+
+        res.status(500).json({
+            message: 'Error occurred while fetching quiz statistics',
+        })
+    }
+})

823a46f7ae5d9eb0a6f8c32512a5f3986fb19fad
AlexJCodes <alexander.johansson8910@gmail.com>
2026-06-03 20:55:34 +0200

    ✨ feat: add question detail endpoint

--- src/routes/questions.routes.ts
+++ src/routes/questions.routes.ts
@@ -1,5 +1,5 @@
 import express, { type Request, type Response } from 'express'
-import { getAllQuestions } from '../repositories/questionsRepository'
+import { getAllQuestions, getQuestionById } from '../repositories/questionsRepository'
 import { createErrorResponse } from '../utils/apiResponse'
 
 export const questionsRouter = express.Router()
@@ -7,6 +7,7 @@ export const questionsRouter = express.Router()
 // ----------------------------------------------------------- //
 // ------------------------ GET ------------------------------ //
 // ----------------------------------------------------------- //
+
 questionsRouter.get('/', async (_req: Request, res: Response) => {
 	try {
 		const questions = await getAllQuestions()
@@ -16,5 +17,27 @@ questionsRouter.get('/', async (_req: Request, res: Response) => {
 		console.error(error)
 
 		res.status(500).json(createErrorResponse('Error occured while fetching questions'))
+	}
+})
+
+// ----------------------------------------------------------- //
+// ---------------------- GET BY ID -------------------------- //
+// ----------------------------------------------------------- //
+
+questionsRouter.get('/:id', async (req: Request<{ id: string }>, res: Response) => {
+	try {
+		const { id } = req.params
+
+		const question = await getQuestionById(id)
+
+		if (!question) {
+			return res.status(404).json(createErrorResponse(`Question with id ${id} was not found`))
+		}
+
+		res.json(question)
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json(createErrorResponse('Error occurred while fetching question'))
 	}
 })
